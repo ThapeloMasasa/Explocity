0fd6157b1136483ac6a8c200ff29894cc9cf0027
ThapeloMasasa <tmasasa@nd.edu>
2023-03-15 12:29:52 -0400

    Maps working

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+
+const URL = 'https://travel-advisor.p.rapidapi.com/hotels/list-in-boundary';
+
+   
+export const getPlacesData = async (sw, ne) =>{
+    try{
+        console.log('no error')
+        const {data: {data}}= await axios.get(URL,{
+            params: {
+                bl_latitude: sw.lat,
+                bl_longitude: ne.lng,
+                tr_longitude: sw.lng,
+                tr_latitude: ne.lat,
+              },
+              headers: {
+                'X-RapidAPI-Key': 'dded7e27cdmsh0918d1cbed4f915p1e9e6ejsn5e49267792ad',
+                'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+              }
+            }
+            );
+        return data
+    }catch(error){
+        console.log(error)
+    }
+}
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import GoogleMapReact from 'google-map-react';
+import {Paper, Typography, useMediaQuery} from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import useStyles from './styles';
+
+const Map = ({setCoordinates, setBounds, coordinates}) => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    return(
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key:'AIzaSyD_pwyKRVp7at1mtryDxVetG0zWAQmdC7c'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={(e)=>{
+                    setCoordinates({lat: e.center.lat, lng: e.center.lng});
+                    setBounds({ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+                }}
+                >
+
+            </GoogleMapReact>
+        </div>
+    );
+}
+
+export default Map
